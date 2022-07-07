4f907090e8f66cd8246df50c011625a33bcee638
watida28 <106501284+watida28@users.noreply.github.com>
2022-07-07 20:03:40 +0700

    test

--- test.py
+++ test.py
@@ -1,1 +1,21 @@
-class 
+from abc import ABC, abstractclassmethod, abstractmethod
+
+class Country(ABC):
+    def init(self,name,population) -> None:
+        super().init()
+        self.name = name
+        self.population = population
+
+    @abstractmethod
+    def show_detail(self):
+        passfrom abc import ABC, abstractclassmethod, abstractmethod
+
+class Country(ABC):
+    def init(self,name,population) -> None:
+        super().init()
+        self.name = name
+        self.population = population
+
+    @abstractmethod
+    def show_detail(self):
+        pass
